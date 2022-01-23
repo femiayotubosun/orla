8d5665974253b42f0638647b26829d8e8ddf3cab
Femi Ayo-Tubosun <femiayotubosun@gmail.com>
2022-01-23 06:25:04 +0100

    Adding docker configuration

---
+++ Dockerfile
@@ -0,0 +1,38 @@
+# pull official base image
+FROM python:3.10-alpine
+
+# set work directory
+WORKDIR /app
+
+
+# set environment variables
+ENV PYTHONDONTWRITEBYTECODE 1
+ENV PYTHONUNBUFFERED 1
+ENV DEBUG 0
+
+# install psycopg2
+RUN apk update \
+    && apk add --virtual build-essential gcc python3-dev musl-dev \
+    && apk add postgresql-dev \
+    && pip install psycopg2 \
+    && apk add libffi \
+    && apk add libffi-dev             
+
+RUN apk add --update --no-cache --virtual .tmp libc-dev linux-headers zlib-dev jpeg-dev \
+    && apk add libjpeg \ 
+    && pip install Pillow --no-cache-dir \ 
+    && apk del .tmp
+
+
+# install pipenv and install dependencies
+COPY ./requirements.txt .
+RUN pip install -r requirements.txt
+# copy project
+COPY . .
+
+# add and run as non-root user
+RUN adduser -D myuser
+USER myuser
+
+# run gunicorn
+CMD gunicorn orla.wsgi:application --bind 0.0.0.0:$PORT
--- orla/settings.py
+++ orla/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import os
 from dotenv import load_dotenv
+import dj_database_url
 
 load_dotenv()
 
@@ -48,6 +49,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",  # new
     "django.contrib.sessions.middleware.SessionMiddleware",
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -89,6 +91,12 @@ DATABASES = {
     }
 }
 
+DATABASE_URL = os.environ.get("DATABASE_URL")
+db_from_env = dj_database_url.config(
+    default=DATABASE_URL, conn_max_age=500, ssl_require=True
+)
+DATABASES["default"].update(db_from_env)
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
@@ -125,8 +133,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 MEDIA_URL = "media/"
 MEDIA_ROOT = BASE_DIR / "media"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
